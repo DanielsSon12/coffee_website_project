6ac9f46f0981082804b3d816cd225c8c6f2b46bf
Daniel_Ouver <DANIELOUVERNEY67@GMAIL.COM>
2026-08-09 18:29:48 -0300

    style: atualiza estilização dos cards e do carrosel

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -10,18 +10,31 @@ const CoffeeList = ({ loading, coffee }: any) => {
 			<div className="m-auto p-8 text-center">
 				<h1>Coffee Website</h1>
 			</div>
-			<div className="m-auto flex w-xl items-center justify-center overflow-x-auto">
+			<div className="m-auto flex w-6xl cursor-grab items-center overflow-x-auto scroll-smooth">
 				{coffee.map((item: any) => (
-					<div className="m-5 rounded-md bg-amber-100 p-10" key={item.id}>
-						<img className="min-h-full w-full" src={item.image} alt={item.title} />
-						<h2>{item.title}</h2>
-						<p>{item.description}</p>
+					<div
+						className="m-5 flex h-auto w-auto flex-none cursor-grab rounded-2xl bg-amber-100 p-0"
+						key={item.id}
+					>
 						<div>
-							<ul>
-								{item.ingredients.map((ingredient: any) => (
-									<li>{ingredient}</li>
-								))}
-							</ul>
+							<img
+								className="h-105 w-xs rounded-l-2xl object-cover"
+								src={item.image}
+								alt={item.title}
+							/>
+						</div>
+						<div>
+							<h2 className="mt-5 text-center text-2xl font-bold text-amber-900">{item.title}</h2>
+							<p className="text-md m-5 w-60 tracking-wider text-gray-800">{item.description}</p>
+							<div className="m-5">
+								<ul className="list-none">
+									{item.ingredients.map((ingredient: any) => (
+										<li className="mt-2 text-sm font-bold tracking-wider text-amber-800">
+											➜ {ingredient}
+										</li>
+									))}
+								</ul>
+							</div>
 						</div>
 					</div>
 				))}
