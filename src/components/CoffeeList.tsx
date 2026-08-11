eefc17435a7185b3fc517fa54f4bc689e3a2215d
Daniel_Ouver <DANIELOUVERNEY67@GMAIL.COM>
2026-08-10 22:32:52 -0300

    feat: adiciona um carrosel arrastável com o framer motion

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -1,6 +1,23 @@
+import { motion } from 'framer-motion'
+import { useEffect, useRef, useState } from 'react'
+
 import withDataFetch from './DataFetching'
 
 const CoffeeList = ({ loading, coffee }: any) => {
+	const carousel = useRef<HTMLDivElement>(null)
+	const innerCarousel = useRef<HTMLDivElement>(null)
+
+	const [width, setWidth] = useState(0)
+
+	useEffect(() => {
+		if (!carousel.current || !innerCarousel.current) return
+
+		const containerWidth = carousel.current.offsetWidth
+		const contentWidth = innerCarousel.current.scrollWidth
+
+		setWidth(contentWidth - containerWidth)
+	}, [coffee])
+
 	if (loading) {
 		return <h1>Carregando...</h1>
 	}
@@ -10,35 +27,49 @@ const CoffeeList = ({ loading, coffee }: any) => {
 			<div className="m-auto p-8 text-center">
 				<h1>Coffee Website</h1>
 			</div>
-			<div className="m-auto flex w-7xl cursor-grab items-center overflow-x-auto scroll-smooth">
-				{coffee.map((item: any) => (
-					<div
-						className="m-5 flex h-auto w-auto flex-none cursor-grab rounded-2xl bg-amber-100 p-0"
-						key={item.id}
-					>
-						<div>
-							<img
-								className="h-105 w-xs rounded-l-2xl object-cover"
-								src={item.image}
-								alt={item.title}
-							/>
-						</div>
-						<div>
-							<h2 className="mt-5 text-center text-2xl font-bold text-amber-900">{item.title}</h2>
-							<p className="text-md m-5 w-60 tracking-wider text-gray-800">{item.description}</p>
-							<div className="m-5">
-								<ul className="list-none">
-									{item.ingredients.map((ingredient: any) => (
-										<li className="mt-2 text-sm font-bold tracking-wider text-amber-800">
-											➜ {ingredient}
-										</li>
-									))}
-								</ul>
+			<motion.div
+				ref={carousel}
+				className="m-auto flex w-full max-w-7xl items-center overflow-hidden scroll-smooth"
+			>
+				<motion.div
+					ref={innerCarousel}
+					className="flex cursor-grab"
+					drag="x"
+					dragConstraints={{ right: 0, left: -width }}
+					dragElastic={0}
+					whileTap={{ cursor: 'grabbing' }}
+				>
+					{coffee.map((item: any) => (
+						<motion.div
+							key={item.id}
+							className="m-5 flex h-auto w-auto flex-none cursor-grab rounded-2xl bg-amber-100 p-0"
+							whileTap={{ cursor: 'grabbing' }}
+						>
+							<div>
+								<img
+									draggable={false}
+									className="pointer-events-none h-105 w-full max-w-xs rounded-l-2xl object-cover"
+									src={item.image}
+									alt={item.title}
+								/>
 							</div>
-						</div>
-					</div>
-				))}
-			</div>
+							<div>
+								<h2 className="mt-5 text-center text-2xl font-bold text-amber-900">{item.title}</h2>
+								<p className="text-md m-5 w-60 tracking-wider text-gray-800">{item.description}</p>
+								<div className="m-5">
+									<ul className="list-none">
+										{item.ingredients.map((ingredient: any) => (
+											<li className="mt-2 text-sm font-bold tracking-wider text-amber-800">
+												➜ {ingredient}
+											</li>
+										))}
+									</ul>
+								</div>
+							</div>
+						</motion.div>
+					))}
+				</motion.div>
+			</motion.div>
 		</main>
 	)
 }
